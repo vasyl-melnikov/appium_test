457ed335a92e3eed1fde54283d301560316dca59
vasyl <vasyl.melnkv@gmail.com>
2023-07-23 20:48:14 +0300

    Add new ui elements for representing each object, add class Keys for better representing integer button keys

--- tripadvisor_parser/ui_elements.py
+++ tripadvisor_parser/ui_elements.py
@@ -1,5 +1,29 @@
 class UiElements:
+    day_view = "com.tripadvisor.tripadvisor:id/dayView"
+    day_txt = "com.tripadvisor.tripadvisor:id/txtDay"
+    cur_date_title = "com.tripadvisor.tripadvisor:id/txtTitle"
+    confirm_date_range_btn = "com.tripadvisor.tripadvisor:id/btnPrimary"
+    date_range_page_btn = "com.tripadvisor.tripadvisor:id/txtDate"
+    back_from_deals_page_btn = "com.tripadvisor.tripadvisor:id/barBtnGlobalNavPrimary"
+    search_page_btn = "com.tripadvisor.tripadvisor:id/tab_search"
+    cancel_invalid_date_range_popup = "com.tripadvisor.tripadvisor:id/circularBtnIconSmallCancel"
+    search_page_edit_prompt = "com.tripadvisor.tripadvisor:id/edtSearchString"
+    search_result_object = "com.tripadvisor.tripadvisor:id/card"
+    all_deals_page_btn = "com.tripadvisor.tripadvisor:id/btnAllDeals"
+    hotel_offer_object = "com.tripadvisor.tripadvisor:id/cardHotelOffer"
+    hotel_offer_provider = "com.tripadvisor.tripadvisor:id/txtProviderName"
+    hotel_offer_price = "com.tripadvisor.tripadvisor:id/txtPriceTopDeal"
+    month_view_object = "com.tripadvisor.tripadvisor:id/monthView"
+    date_range_final_date = "com.tripadvisor.tripadvisor:id/txtStartInputText"
     cancel_google_login_page = 'com.google.android.gms:id/cancel'
     skip_trapadvisor_login_page = 'com.tripadvisor.tripadvisor:id/bdlBtnSkip'
     skip_page_btn = 'com.tripadvisor.tripadvisor:id/bdlBtnNotNow'
     trapadvisor_app_element = '//android.widget.TextView[@content-desc="Tripadvisor"]'
+
+
+class Keys:
+    enter = 66
+    tab = 61
+    arrow_up = 19
+    arrow_down = 20
+    arrow_left = 21
