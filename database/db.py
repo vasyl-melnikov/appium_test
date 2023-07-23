4374adce00b3af6b3a147d2305ace22adca30f45
vasyl <vasyl.melnkv@gmail.com>
2023-07-23 21:44:26 +0300

    Remove all unnecessary things form db file, change db_url to postgresql

--- database/db.py
+++ database/db.py
@@ -1,28 +1,11 @@
-from typing import Optional
-
-from sqlmodel import Field, SQLModel, create_engine, select, Session
+from sqlmodel import create_engine
 
 
-class Task(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    status: str
-    result_body: str
+db_url = "postgresql://my_user:my_password@localhost:5432/my_database"
+engine = create_engine(db_url)
 
 
-sqlite_url = "postgresql://testuser:testuser@localhost:5432/testdb"
-
-engine = create_engine(sqlite_url, echo=True)
-def create_task():
-    with Session(engine) as session:
-        new_task = Task(status='done', result_body='here we are')
-        session.add(new_task)
-        session.commit()
-def read_task():
-    with Session(engine) as session:
-        statement = select(Task)
-        results = session.exec(statement)
-        print(results)
-if __name__ == "__main__":
-    # SQLModel.metadata.create_all(engine)
-    # create_task()
-    read_task()
+# if __name__ == "__main__":
+#     # SQLModel.metadata.create_all(engine)
+#     # create_task()
+#     # read_task()
