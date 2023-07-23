132f86d3f2256aecf9b312a4f94bc15bdae70488
vasyl <vasyl.melnkv@gmail.com>
2023-07-23 21:45:16 +0300

    Change model type to Optional[str]

--- database/models.py
+++ database/models.py
@@ -6,4 +6,4 @@ from sqlmodel import Field, SQLModel
 class Task(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     status: str
-    result_body: str
+    result_body: Optional[str]
