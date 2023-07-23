61a572a55b18d7a3faa58fa539e4cec61082863f
vasyl <vasyl.melnkv@gmail.com>
2023-07-23 21:44:51 +0300

    Implement api methods

--- api/api.py
+++ api/api.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
+from sqlmodel import Session, select
+from database.db import engine
+from database.models import Task
 
 app = FastAPI()
 
-@app.post('/tasks/create_task')
+
+@app.post('/tasks/')
 def create_task():
     pass
 
-@app.get('/task_status/{task_id}')
-def get_task(task_id: str):
-    pass
 
-@app.get('/tasks')
-def get_task(task_id: str):
-    pass
+@app.get('/tasks/{task_id}', response_model=Task)
+def get_task(task_id: int):
+    with Session(engine) as session:
+        return session.get(Task, task_id)
+
+
+@app.get('/tasks/', response_model=list[Task])
+def get_all_tasks():
+    with Session(engine) as session:
+        return session.exec(select(Task)).all()
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
