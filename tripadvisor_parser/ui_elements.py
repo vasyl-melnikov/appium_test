57509100b5f1c57ffa935160c3897eee1c276e63
vasyl <vasyl.melnkv@gmail.com>
2023-07-23 20:44:42 +0300

    Create UiElements class to be as data class to contain string representation for ui elements

---
+++ tripadvisor_parser/ui_elements.py
@@ -0,0 +1,5 @@
+class UiElements:
+    cancel_google_login_page = 'com.google.android.gms:id/cancel'
+    skip_trapadvisor_login_page = 'com.tripadvisor.tripadvisor:id/bdlBtnSkip'
+    skip_page_btn = 'com.tripadvisor.tripadvisor:id/bdlBtnNotNow'
+    trapadvisor_app_element = '//android.widget.TextView[@content-desc="Tripadvisor"]'
