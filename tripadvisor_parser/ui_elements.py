119237612da99f1f03ea0fe9da87e7c0bc200ca0
vasyl <vasyl.melnkv@gmail.com>
2023-07-25 21:23:47 +0300

    Change ui element to fix bug with exception raised because content-description changed

--- tripadvisor_parser/ui_elements.py
+++ tripadvisor_parser/ui_elements.py
@@ -20,7 +20,7 @@ class UiElements:
     cancel_google_login_page = "com.google.android.gms:id/cancel"
     skip_trapadvisor_login_page = "com.tripadvisor.tripadvisor:id/bdlBtnSkip"
     skip_page_btn = "com.tripadvisor.tripadvisor:id/bdlBtnNotNow"
-    trapadvisor_app_element = '//android.widget.TextView[@content-desc="Tripadvisor"]'
+    trapadvisor_app_element = '//android.widget.TextView[@text="Tripadvisor"]'
 
 
 class Keys:
