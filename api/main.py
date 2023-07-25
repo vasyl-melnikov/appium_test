dba074ab84d79f2acdd2e0c235ed27dc1305892a
vasyl <vasyl.melnkv@gmail.com>
2023-07-25 21:25:15 +0300

    Change package name, change host for web api

--- api/main.py
+++ api/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
-from api import tasks
+import tasks_api
 
 app = FastAPI()
-app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
+app.include_router(tasks_api.router, prefix="/tasks", tags=["tasks"])
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0")
